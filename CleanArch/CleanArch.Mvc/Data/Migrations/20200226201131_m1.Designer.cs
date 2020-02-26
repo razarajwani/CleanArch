788c36d2029f69d57d58de35fbbf2a925f9769fc
Raza <razarajwani@live.com>
2020-02-26 23:19:13 +0300

    Test

--- CleanArch/CleanArch.Mvc/Data/Migrations/20200226201131_m1.Designer.cs
+++ CleanArch/CleanArch.Mvc/Data/Migrations/20200226201131_m1.Designer.cs
@@ -4,14 +4,16 @@ using CleanArch.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArch.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200226201131_m1")]
+    partial class m1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
